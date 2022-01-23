0a007eb04c85f0fac58af3d591c07ea5021f2a55
Alex Daily <alex@cozero.io>
2022-01-23 16:26:57 +0100

    additional services

---
+++ src/api/apps/crud-service/src/items/dto/create-item.dto.ts
@@ -0,0 +1,7 @@
+import { Prisma } from '@prisma/client'
+
+export type CreateItemDto = Prisma.ItemCreateInput
+
+// TODO extend prisma types directly
+
+//export class : Test {}
---
+++ src/api/apps/crud-service/src/items/dto/update-item.dto.ts
@@ -0,0 +1,3 @@
+import { CreateItemDto } from './create-item.dto';
+
+export type UpdateItemDto = Partial<CreateItemDto>
