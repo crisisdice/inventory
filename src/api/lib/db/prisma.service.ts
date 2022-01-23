36d7af66b392b5ae525f963339baf750666b0ebd
Alex Daily <alex@cozero.io>
2022-01-23 14:21:05 +0100

    init db

---
+++ src/api/lib/db/prisma.service.ts
@@ -0,0 +1,24 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common'
+import { PrismaClient, Prisma } from '@prisma/client'
+
+@Injectable()
+export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+  constructor() {
+    const log: (Prisma.LogLevel | Prisma.LogDefinition)[] = [
+      { emit: 'stdout', level: 'query' },
+      { emit: 'stdout', level: 'info' },
+      { emit: 'stdout', level: 'warn' },
+      { emit: 'stdout', level: 'error' },
+    ]
+
+    super({ log: process.env.PRISMA_LOG_LEVEL === 'error' ? log : undefined })
+  }
+
+  async onModuleInit(): Promise<void> {
+    await this.$connect()
+  }
+
+  async onModuleDestroy(): Promise<void> {
+    await this.$disconnect()
+  }
+}
